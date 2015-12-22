df054b80fcd1dc00ff8a05905f26a46f2291a40d
kittiu <kittiu@kittiu>
2015-12-22 19:12:55 +0700

    -

--- analytic_multi_dimension/models/account_dimension.py
+++ analytic_multi_dimension/models/account_dimension.py
@@ -1,41 +1,113 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import itertools
+from openerp import api, fields, models, _
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
 
-from openerp import api, fields, models
 
-
-class account_dimension(models.Model):
-    _name = 'account.dimension'
-    _description = 'Account Dimension'
-    _order = 'level'
+class AccountDimensionGroup(models.Model):
+    _name = 'account.dimension.group'
+    _description = 'Dimension Group'
 
     name = fields.Char(
+        string='Dimension Group',
+        required=True,
+    )
+    dimension_ids = fields.One2many(
+        'account.dimension.group.line',
+        'group_id',
+        string='Dimensions',
+    )
+
+    @api.one
+    @api.constrains('dimension_ids')
+    def _check_lines(self):
+        lines = self.dimension_ids.filtered(lambda r: r.activity is True)
+        if len(lines) != 1:
+            raise ValidationError(_('A group must have 1 activity dimension'))
+
+
+class AccountDimensionGroupLine(models.Model):
+    _name = 'account.dimension.group.line'
+    _description = 'Dimension Group Lines'
+    _order = 'sequence, id'
+
+    group_id = fields.Many2one(
+        'account.dimension.group',
+        string='Dimension',
+        ondelete='restrict',
+    )
+    sequence = fields.Integer(
+        string='Sequence',
+        default=1,
+    )
+    dimension_id = fields.Many2one(
+        'account.dimension',
         string='Dimension',
         required=True,
     )
-    level = fields.Selection(
-        [('1', '1'),
-         ('2', '2'),
-         ('3', '3'),
-         ('4', '4'),
-         ('5', '5'),
-         ('6', '6'),
-         ],
-        string='Level',
+    activity = fields.Boolean(
+        related='dimension_id.activity',
+        string='Activity',
+        readonly='True',
+    )
+    _sql_constraints = [
+        ('dimension_uniq',
+         'unique(group_id, dimension_id)',
+         'Dimensions must be unique per group!'),
+    ]
+
+
+class AccountDimension(models.Model):
+    _name = 'account.dimension'
+    _description = 'Account Dimension'
+
+    DIMENSIONS = [('d1', '01'), ('d2', '02'), ('d3', '03'),
+                  ('d4', '04'), ('d5', '05'), ('d6', '06'),
+                  ('d7', '07'), ('d8', '08'), ('d9', '09'),
+                  ('d10', '10'),
+                  ]
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+    code = fields.Selection(
+        DIMENSIONS,
+        string='Dimension',
+        required=True,
     )
     activity = fields.Boolean(
-        string='Activity Level',
+        string='Activity',
         default=False,
     )
-    line_ids = fields.One2many(
-        'account.dimension.line',
+    item_ids = fields.One2many(
+        'account.dimension.item',
         'dimension_id',
         string='Dimension Lines',
     )
+    group_ids = fields.Many2many(
+        'account.dimension.group',
+        string='Used in Groups',
+        compute='_compute_group_ids',
+    )
+    _sql_constraints = [
+        ('dimension_code_uniq',
+         'unique(code)',
+         'Dimension ID must be unique!'),
+    ]
+
+    @api.multi
+    def _compute_group_ids(self):
+        for dimension in self:
+            lines = self.env['account.dimension.group.line'].search(
+                [('dimension_id', '=', dimension.id)])
+            group_ids = [x.group_id.id for x in lines]
+            dimension.group_ids = group_ids
 
 
-class account_dimension_line(models.Model):
-    _name = 'account.dimension.line'
+class AccountDimensionItem(models.Model):
+    _name = 'account.dimension.item'
     _description = 'Account Dimension'
 
     dimension_id = fields.Many2one(
@@ -47,4 +119,42 @@ class account_dimension_line(models.Model):
         string='Dimension',
         required=True,
     )
+
+#     @api.model
+#     def create(self, vals):
+#         new_item = super(AccountDimensionItem, self).create(vals)
+#         new_item.create_analytic_account_activity()
+#         return new_item
+
+    @api.multi
+    def create_analytic_account_activity(self):
+        """ This method create all possible comination in analytic account
+            It is not being used at the moment, and there will be too many
+            combination
+        """
+        for item in self:
+            if not item.dimension_id.activity:
+                continue
+            dimension_groups = item.dimension_id.group_ids
+            for group in dimension_groups:
+                comb_list = []
+                for line in group.dimension_ids:
+                    if line.dimension_id.activity:
+                        continue
+                    comb_list.append(
+                        (line.dimension_id.code,
+                         [x.id for x in line.dimension_id.item_ids]))
+                # Given dict, i.e., [('d2', [4, 5, 6]), ('d3', [7, 8])]
+                # Get all possible combinations => [(4, 7), (5, 7), (6, 7)] ...
+                dimensions, values = zip(*comb_list)   # Split into 2 list
+                map(lambda x: x.append(False), values)
+                combinations = list(itertools.product(*values))
+                # Create analytic account for every possible combination
+                for comb in combinations:
+                    vals = dict(zip(dimensions, comb))
+                    vals[item.dimension_id.code] = item.id
+                    vals['dimension_group_id'] = group.id
+                    vals['name'] = item.name
+                    self.env['account.analytic.account'].create(vals)
+        return dict
     
--- analytic_multi_dimension/models/analytic.py
+++ analytic_multi_dimension/models/analytic.py
@@ -1,48 +1,118 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from lxml import etree
 from openerp import api, fields, models
+from openerp.osv.orm import setup_modifiers
 
 
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
+    dimension_group_id = fields.Many2one(
+        'account.dimension.group',
+        string='Dimension Group',
+    )
+    # Active Flag
+    d1_active = fields.Boolean(compute='_compute_dimension_active')
+    d2_active = fields.Boolean(compute='_compute_dimension_active')
+    d3_active = fields.Boolean(compute='_compute_dimension_active')
+    d4_active = fields.Boolean(compute='_compute_dimension_active')
+    d5_active = fields.Boolean(compute='_compute_dimension_active')
+    d6_active = fields.Boolean(compute='_compute_dimension_active')
+    d7_active = fields.Boolean(compute='_compute_dimension_active')
+    d8_active = fields.Boolean(compute='_compute_dimension_active')
+    d9_active = fields.Boolean(compute='_compute_dimension_active')
+    d10_active = fields.Boolean(compute='_compute_dimension_active')
+    # --
     d1 = fields.Many2one(
-        'account.dimension.line',
-        string='Org',
-        domain=[('dimension_id.level', '=', '1')],
+        'account.dimension.item',
+        string='Dimension 1',
+        domain="[('dimension_id.code', '=', 'd1')]",
         ondelete='set null',
     )
     d2 = fields.Many2one(
-        'account.dimension.line',
-        string='Dept',
-        domain=[('dimension_id.level', '=', '2')],
+        'account.dimension.item',
+        string='Dimension 2',
+        domain="[('dimension_id.code', '=', 'd2')]",
         ondelete='set null',
     )
     d3 = fields.Many2one(
-        'account.dimension.line',
-        string='Programme',
-        domain=[('dimension_id.level', '=', '3')],
+        'account.dimension.item',
+        string='Dimension 3',
+        domain="[('dimension_id.code', '=', 'd3')]",
         ondelete='set null',
     )
     d4 = fields.Many2one(
-        'account.dimension.line',
-        string='Project',
-        domain=[('dimension_id.level', '=', '4')],
+        'account.dimension.item',
+        string='Dimension 4',
+        domain="[('dimension_id.code', '=', 'd4')]",
         ondelete='set null',
     )
     d5 = fields.Many2one(
-        'account.dimension.line',
-        string='Activity Group',
-        domain=[('dimension_id.level', '=', '5')],
+        'account.dimension.item',
+        string='Dimension 5',
+        domain="[('dimension_id.code', '=', 'd5')]",
         ondelete='set null',
     )
     d6 = fields.Many2one(
-        'account.dimension.line',
-        string='Activity',
-        domain=[('dimension_id.level', '=', '6')],
+        'account.dimension.item',
+        string='Dimension 6',
+        domain="[('dimension_id.code', '=', 'd6')]",
+        ondelete='set null',
+    )
+    d7 = fields.Many2one(
+        'account.dimension.item',
+        string='Dimension 7',
+        domain="[('dimension_id.code', '=', 'd7')]",
+        ondelete='set null',
+    )
+    d8 = fields.Many2one(
+        'account.dimension.item',
+        string='Dimension 8',
+        domain="[('dimension_id.code', '=', 'd8')]",
+        ondelete='set null',
+    )
+    d9 = fields.Many2one(
+        'account.dimension.item',
+        string='Dimension 9',
+        domain="[('dimension_id.code', '=', 'd9')]",
+        ondelete='set null',
+    )
+    d10 = fields.Many2one(
+        'account.dimension.item',
+        string='Dimension 10',
+        domain="[('dimension_id.code', '=', 'd10')]",
         ondelete='set null',
     )
     _sql_constraints = [
-        ('dimension_uniq', 'unique(d1, d2, d3, d4, d5, d6)', 'Analytic Account Dimensions must be unique!'),
+        ('dimension_uniq',
+         'unique(d1, d2, d3, d4, d5, d6, d7, d8, d9, d10)',
+         'Analytic Account Dimensions must be unique!'),
     ]
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        result = super(AccountAnalyticAccount, self).fields_view_get(view_id, view_type, toolbar=toolbar, submenu=submenu)
+        doc = etree.XML(result['arch'])
+        dimensions = [(x.code, x.name) for x in
+                      self.env['account.dimension'].search([])]
+        for d in dimensions:
+            nodes = doc.xpath("//field[@name='%s']" % d[0])
+            if nodes:
+                node = nodes[0]
+                node.set('string', d[1])
+                setup_modifiers(node, result['fields'][d[0]])
+        result['arch'] = etree.tostring(doc)
+        print result
+        return result
+
+    @api.multi
+    @api.depends('dimension_group_id')
+    def _compute_dimension_active(self):
+        for analytic in self:
+            codes = [(line.dimension_id.code, line.dimension_id.name) for
+                     line in analytic.dimension_group_id.dimension_ids]
+            for d in self.env['account.dimension'].DIMENSIONS:
+                analytic[d[0] + '_active'] = False
+            for d in codes:
+                analytic[d[0] + '_active'] = True
